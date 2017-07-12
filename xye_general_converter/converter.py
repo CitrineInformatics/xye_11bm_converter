82656240badabd939457f959b5e50f2b5422eff4
sbajaj <saurabhbajaj2@gmail.com>
2017-07-12 10:58:34 -0700

    finish core of the converter

--- xye_general_converter/converter.py
+++ xye_general_converter/converter.py
@@ -1,4 +1,4 @@
-from pypif.obj import ChemicalSystem
+from pypif.obj import *
 
 
 def convert(files=[], important_argument=None):
@@ -13,11 +13,49 @@ def convert(files=[], important_argument=None):
     # only expecting 1 file
     assert len(files) == 1
 
+    chem_sys = ChemicalSystem()
+    chem_sys.properties = []
+
     # Read in the whole database
     with open(files[0], 'r') as f:
         lines = f.readlines()
 
-    print lines
+    # Remove metadata lines
+    lines = [line for line in lines if line[0] not in ['/', '#', '*', "'"]]
+
+    x = []
+    y = []
+    e = []
+
+    for line in lines:
+        print line
+        x_y_e = line.split()
+
+        # If length of line is not 3, skip line
+        if len(x_y_e) != 3:
+            continue
+
+        # Even if one of the numbers is not convertible to float, skip line
+        try:
+            floats = [float(num) for num in x_y_e]
+        except ValueError:
+            continue
+
+        x.append(Scalar(value=x_y_e[0]))
+        y.append(Scalar(value=x_y_e[1], uncertainty=x_y_e[2]))
+
+    two_theta = Property(name="2$\theta$", scalars=x, units='degrees')
+    intensity = Property(name="Intensity", scalars=y)
+
+    chem_sys.properties.append(two_theta)
+    chem_sys.properties.append(intensity)
+
+
+
+
+    # print len(lines)
+    # print lines[2502]
+    # print lines[-1:]
 
 if __name__ == "__main__":
     convert(files=["../test_files/LuFe2O4_700Air_hold3-00059.xye"])
